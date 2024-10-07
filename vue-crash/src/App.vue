171c175e8dffe045b9262e2b8c467d0e9552a8aa
veeto <killersauros01@gmail.com>
2024-10-07 20:58:32 +0100

    feat: using composition

--- vue-crash/src/App.vue
+++ vue-crash/src/App.vue
@@ -1,25 +1,43 @@
-<script>
-export default {
-  data() {
-    return {name:'John Doe', status: 'pending',
-      tasks: ['task1', 'task2', 'task3'],
-      link: 'https://google.com'
-    };
-  },
-  methods: {
-    toggleStatus() {
-      if(this.status == 'active') {
-        this.status = 'pending';
-      }else if(this.status == 'pending'){
-        this.status = 'inactive';
+<script setup>
+
+import { ref,onMounted } from 'vue';
+
+    const name = ref('john doe');
+    const status= ref('active');
+    const tasks = ref(['task1', 'task2', 'task3']);
+    const link = 'http://google.com';
+    const newTask = ref('dsdsa');
+  
+    const toggleStatus = () => {
+      if(status.value == 'active') {
+        status.value = 'pending';
+      }else if(status.value == 'pending'){
+        status.value = 'inactive';
       }else{
-        this.status = 'active';
+        status.value = 'active';
       }
+    };
+
+    const addTask = () => {
+      if(newTask.value.trim() !== '' ) {
+        tasks.value.push(newTask.value);
+        newTask.value = '';
+      }
+    };
+
+    const deleteTask = (index) => {
+      tasks.value.splice(index, 1);
     }
-  }
-}
 
-
+    onMounted(async () => {
+      try {
+        const response = await fetch('https://jsonplaceholder.typicode.com/todos');
+        const data = await response.json();
+        tasks.value  = data.map(task => task.title);
+      } catch (e) {
+        console.log('Error fetching task' + e);
+      }
+    })
 </script>
 
 <template>
@@ -28,9 +46,19 @@ export default {
   <p v-else-if="status === 'pending'">User is pending</p>
   <p v-else>User is inactive</p>
 
+  <form @submit.prevent="addTask">
+    <lable for="newTask">Add Task:</lable>
+    <input type="text" id="newTask" name="newTask" v-model="newTask">
+    <button type="submit">Add new Task</button>
+  </form> 
+
+
+
   <h3>Tasks:</h3>
   <ul>
-    <li v-for="task in tasks" :key="task" >{{ task }}</li>
+    <li v-for="(task,index) in tasks" :key="task" >
+      <span>{{ task }}</span>
+    <button @click="deleteTask(index)"> x</button></li>
   </ul>
   <a v-bind:href="link">Click for google</a>
 
---
+++ vue-crash/src/App_v2.vue
@@ -0,0 +1,42 @@
+<script>
+export default {
+  data() {
+    return {name:'John Doe', status: 'pending',
+      tasks: ['task1', 'task2', 'task3'],
+      link: 'https://google.com'
+    };
+  },
+  methods: {
+    toggleStatus() {
+      if(this.status == 'active') {
+        this.status = 'pending';
+      }else if(this.status == 'pending'){
+        this.status = 'inactive';
+      }else{
+        this.status = 'active';
+      }
+    }
+  }
+}
+
+
+</script>
+
+<template>
+  <h1>Hello {{ name }}</h1>
+  <p v-if="status === 'active'">User is active</p>
+  <p v-else-if="status === 'pending'">User is pending</p>
+  <p v-else>User is inactive</p>
+
+  <h3>Tasks:</h3>
+  <ul>
+    <li v-for="task in tasks" :key="task" >{{ task }}</li>
+  </ul>
+  <a v-bind:href="link">Click for google</a>
+
+
+  <!-- <button v-on:click="toggleStatus">Change Status</button> -->
+  <button @click="toggleStatus">Change Status</button>
+</template>
+
+
